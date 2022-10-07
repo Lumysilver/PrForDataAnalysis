6d0ce2c00d8774c2f3aceafed75c1656e84f259d
Luminita Done <lumysilver@gmail.com>
2022-10-07 18:24:01 +0100

    Initial push

---
+++ week1/calcAverage.py
@@ -0,0 +1,8 @@
+#calcAverage
+
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] #we first create a list
+for i in list: #I will go in every element of my list; i is also used for index. we want to iterate over that list, for loops are also used for iteration over each element of the list
+    print(i)# meaning I will go in  and print every element of my list
+    print(i*i) #print i square i
+    print()
+print("Loop over")
